e4b1da8d1b73b2bed9b123fae159d94025d0d594
Le Luc <leluc@Les-MacBook-Pro.local>
2021-02-09 00:29:25 +0900

    register-login-userinfo

---
+++ src/app/Http/Controllers/ApiUserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ApiRegisterRequest;
+use App\Http\Requests\ApiLoginRequest;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class ApiUserController extends Controller
+{
+    //
+    public function register(ApiRegisterRequest $request){
+        $user = new User;
+        $user->fill($request->all());
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return response()->json($user);
+    }
+
+    public function login(ApiLoginRequest $request){
+        if (Auth::attempt([
+            'email'=>$request->email,
+            'password'=>$request->password
+        ])){
+            $user = User::whereEmail($request->email)->first();
+            $user->token = $user->createToken('App')->accessToken;
+            return response()->json($user);
+        }
+        return response()->json(['email'=>'Sai ten truy cap hoac mat khau'],401);
+    }
+    public function userInfo(Request $request){
+        return response()->json($request->user('api'));
+    }
+}
