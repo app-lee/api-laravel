f76cc17a7b514def18b46cfd2adf828043760ffa
Le Luc <leluc@Les-MacBook-Pro.local>
2021-03-11 22:00:46 +0900

    final test

--- src/app/Http/Controllers/ApiUserController.php
+++ src/app/Http/Controllers/ApiUserController.php
@@ -30,7 +30,7 @@ class ApiUserController extends Controller
             $user->token = $user->createToken('App')->accessToken;
             return response()->json($user);
         }
-        return response()->json(['email'=>'Sai ten truy cap hoac mat khau'],401);
+        return response()->json(['mess'=>'Sai ten truy cap hoac mat khau'],401);
     }
     public function userInfo(Request $request){
         return response()->json($request->user('api'));
